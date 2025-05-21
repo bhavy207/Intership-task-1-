f2527089949955a421bba4c1daf9515b1483c6ef
23it057 <23it057@charusat.edu.in>
2025-05-21 09:45:48 +0530

    end point

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function initThreeJS() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
     
+
     // Create renderer
     renderer = new THREE.WebGLRenderer({ 
         alpha: true,
